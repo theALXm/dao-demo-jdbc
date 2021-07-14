06284eae67d7f68b934e9641feadd2c36f1c0973
Alexsandro Morel <alexsandrofreitasmorel@gmail.com>
2021-07-14 17:58:58 -0300

    new classes

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,41 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao
+{
+
+	@Override
+	public void insert(Seller obj) 
+	{
+		
+	}
+
+	@Override
+	public void update(Seller obj)
+	{
+		
+	}
+
+	@Override
+	public void deleteById(Integer Id)
+	{
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) 
+	{
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() 
+	{
+		return null;
+	}
+	
+}
