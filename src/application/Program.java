ef7d4b0843d81edda4f36c8f295ebccaa1f3823f
Alexsandro Morel <alexsandrofreitasmorel@gmail.com>
2021-07-16 14:18:42 -0300

    sla

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 
 public class Program 
@@ -8,7 +10,10 @@ public class Program
 	public static void main(String[] args) 
 	{
 		Department obj = new Department(1, "books");
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+				
 		System.out.println(obj);
+		System.out.println(sellerDao);
 	}
-
 }
